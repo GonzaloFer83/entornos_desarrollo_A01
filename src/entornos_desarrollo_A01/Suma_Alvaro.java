495317856070b0d82d590352b81fe768a2479155
alvaromch <77532797+alvaromch@users.noreply.github.com>
2021-01-19 10:13:30 +0100

    Update Suma_Alvaro.java

--- src/entornos_desarrollo_A01/Suma_Alvaro.java
+++ src/entornos_desarrollo_A01/Suma_Alvaro.java
@@ -1,6 +1,20 @@
 package entornos_desarrollo_A01;
 
+import java.util.Scanner;
+
 public class Suma_Alvaro {
-int a;
-int b;
+		
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		
+		Scanner sc =new Scanner (System.in);
+		a= sc.nextInt();
+		b= sc.nextInt();
+		c= sc.nextInt();
+		
+	}
+	
+		
 }
