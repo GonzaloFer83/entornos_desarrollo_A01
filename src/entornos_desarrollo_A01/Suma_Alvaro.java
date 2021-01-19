cfe99e490eb524cf1a310a18eda73a3d62348e60
alvaromch <77532797+alvaromch@users.noreply.github.com>
2021-01-19 10:39:45 +0100

    Update Suma_Alvaro.java

--- src/entornos_desarrollo_A01/Suma_Alvaro.java
+++ src/entornos_desarrollo_A01/Suma_Alvaro.java
@@ -3,11 +3,16 @@ package entornos_desarrollo_A01;
 import java.util.Scanner;
 
 public class Suma_Alvaro {
-		
+		/**
+		 * 
+		 * Esta clase lo que hace es leer tres numeros de consola y hacer sumas.
+		 * @author Alvaro
+		 */
 	public static void main(String[] args) {
 		int a;
 		int b;
 		int c;
+		
 		
 		Scanner sc =new Scanner (System.in);
 		a= sc.nextInt();
