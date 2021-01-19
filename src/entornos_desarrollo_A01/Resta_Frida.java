c7158b8497f9539d4bb813fafcbbfca6c18f9b7d
Frida-abella <77575398+Frida-abella@users.noreply.github.com>
2021-01-19 11:59:27 +0100

    Update Resta_Frida.java

--- src/entornos_desarrollo_A01/Resta_Frida.java
+++ src/entornos_desarrollo_A01/Resta_Frida.java
@@ -2,9 +2,38 @@ package entornos_desarrollo_A01;
 
 public class Resta_Frida {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	double w;
+	
+	
+	public double restaNumerosReales (double x, double y) {	
+		return w;
 	}
-
+	
+	public double restaNumerosEnteros (int x, int y) { 
+		
+		return w;
+	}
+	
+	public double resta3NumerosReales (double x, double y, double z) {
+		return w;
+		
+	}
+	
+	
+	public double restaValorAcumulado (double z) {
+		return w = w-z;
+	}
+	
+	/**
+	 * Este método es privado dado que sólo se usa dentro de esta clase
+	 * @param i recibe cualquier número entero 
+	 * @return convierte el número entero recibido en un double o número real
+	 */
+	private double convertirEnteros (int i) {
+		return (double) i;
+	}
+	
+	
+	
+	
 }
