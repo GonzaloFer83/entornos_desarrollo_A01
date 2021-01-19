491087924de27a22148e1cde41694e5f021d0bda
alvaromch <77532797+alvaromch@users.noreply.github.com>
2021-01-19 11:32:31 +0100

    Update Suma_Alvaro.java

--- src/entornos_desarrollo_A01/Suma_Alvaro.java
+++ src/entornos_desarrollo_A01/Suma_Alvaro.java
@@ -1,25 +1,50 @@
 package entornos_desarrollo_A01;
 
 import java.util.Scanner;
-
+/**
+ * Ejemplo Javadoc 
+ * 
+ * @since 19/01/2021
+ * @author Alvaro
+ * @version 1.0
+ */
 public class Suma_Alvaro {
-		/**
-		 * 
-		 * Esta clase lo que hace es leer tres numeros de consola y hacer sumas.
-		 * @author Alvaro
-		 */
-	public static void main(String[] args) {
-		int a;
-		int b;
-		int c;
+		double a;
 		
-		
-		Scanner sc =new Scanner (System.in);
-		a= sc.nextInt();
-		b= sc.nextInt();
-		c= sc.nextInt();
-		
+double Suma2NumeroReales(double x, double y){
+			return a;
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public double SumaNumerosEnteros (int x, int y) {
+			return a;
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @return
+	 */
+	public double Suma3NumeroReales(double x,double y,double w){
+			return a;
+	}
+	/**
+	 * Este metodo solo se usa para pasar el valor de entero a double
+	 *
+	 */
+	private double PasarNumerosDouble (int s) {
+			return (double) s;
 	}
 	
-		
+	public double 
+	
+	
+	
 }
+
+
