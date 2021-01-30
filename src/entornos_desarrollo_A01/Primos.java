ff6fa49c10bf51721eed44605ff7e88fc866eb6b
Sergio2600 <77533955+Sergio2600@users.noreply.github.com>
2021-01-30 17:25:47 +0100

    Update Primos.java

--- src/entornos_desarrollo_A01/Primos.java
+++ src/entornos_desarrollo_A01/Primos.java
@@ -1,10 +1,11 @@
 package entornos_desarrollo_A01;
+import entornos_desarrollo_A01.Multiplicacion;
 /**
  * esta clase sirve para realizar numeros primos
  * 
  *@author Sergio Blázquez Fernandez
  *@since 19/01/2021
- *@version 1.0
+ *@version 1.1
  */
 
 public class Primos {
@@ -17,21 +18,34 @@ public class Primos {
 	int t;
 		/**
 		 * 
-		 * @param boolean w
+		 * @param boolean x
 		 * @return esPrimo el cual te dice si el numero es primo o no. 
 		 * @see errorNumerosNegativos()
 	     * @see errorCero()
 		 */
-		public boolean primo(int x) {
-			return esPrimo;
-				
-		}
+	     public boolean esPrimo(int x) {
+					if(x==1 || x%2==0) {
+						return false;
+					}
+					else {
+						for(int i=3; i<Math.sqrt(x);i++) {
+							if(x%i==0) {
+								return true;
+							}
+						}
+					}
+
+					return true;
+		  }		
+		
 		/**
 		 * @param x 
 		 * @return c devuelve la cantidad de numeros primos que hay.
 		 * @see errorNumerosNegativos()
 	     * @see errorCero()
 		 */
+			
+		
 		public int iesimo(int x) {
 			return c;
 		}
@@ -53,39 +67,19 @@ public class Primos {
 		 * @return t devuelve un factorial de un numero entero.
 		 * @see errorNumerosNegativos()
 	     * @see errorCero() 
+	     * @link     -- llamar a la clase Multiplicacion --
 	     * Dentro de este método, comprobaremos que los parámetros introducidos no son negativos o iguales a 0
 	     * llamando a estos métodos privados. 
 		 */
 		
 		public int factorial (int x) {
-			return t;
-		}
-		
-		
-		/**
-		 * Este metodo es privado, dado que solo se usa dentro de esta clase
-		 * Devuelve "Error" en la consola cuando se introduce un numero negativo
-		 * dado el caso de que la calculadora no operase con numeros negativos
-		 * @param i recibe cualquier numero, se comprueba si es negativo
-		 * @return devuelve "Error" si el numero es negativo"
-		 * @deprecated Este metodo podra dejar de ser utilizado con excepciones
-		 */
-		
-		private String errorNumerosNegativos (int i) {
-			return "Error";
-		}
-		
-		
-		/**
-		 * Este metodo es privado, dado que s�lo se usa dentro de esta clase
-		 * Devuelve "Error" por pantalla cuando el numero introducido es igual a O
-		 * dado el caso de que la calculadora no operase con ceros
-		 * @param i recibe cualquier numero, se comprueba si es igual a 0
-		 * @return devuelve "Error"
-		 * @deprecated Este metodo podra dejar de ser utilizado con excepciones
-		 */
-		private String errorCero (int i) {
-			return "Error";
+			if(x==0 || x==1) {
+				return 1;
+			}
+			else {
+				return x*factorial(x-1);
+			}
+
 		}
 			
 		
