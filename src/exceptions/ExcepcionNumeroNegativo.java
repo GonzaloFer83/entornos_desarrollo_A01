d3c0922670c5dc8271aa6f85915288c15eb1542b
Afhsete12 <77535835+Afhsete12@users.noreply.github.com>
2021-02-02 12:46:39 +0100

    Update ExcepcionNumeroNegativo.java

--- src/exceptions/ExcepcionNumeroNegativo.java
+++ src/exceptions/ExcepcionNumeroNegativo.java
@@ -1,7 +1,7 @@
 package exceptions;
 
 public class ExcepcionNumeroNegativo extends ExcepcionParametroNoValido {
-
+	private static final long serialVersionUID = 1L;
 	public ExcepcionNumeroNegativo() {
 		super("No se permite numeros negativos.");
 	}
