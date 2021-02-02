d623bc97e82908c9eceacb0c30d49f1a836c59d6
Afhsete12 <77535835+Afhsete12@users.noreply.github.com>
2021-02-02 12:46:42 +0100

    Update ExcepcionParametroNoValido.java

--- src/exceptions/ExcepcionParametroNoValido.java
+++ src/exceptions/ExcepcionParametroNoValido.java
@@ -3,7 +3,7 @@ import java.lang.Exception;
 
 
 public class ExcepcionParametroNoValido extends Exception {
-	
+	private static final long serialVersionUID = 1L;
 	public ExcepcionParametroNoValido(String error) {
 		super(error);
 	}
