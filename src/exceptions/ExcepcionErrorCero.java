f7e072a9ffa1301a8081e6e6eb2ed19d73a3992b
Afhsete12 <77535835+Afhsete12@users.noreply.github.com>
2021-02-02 12:46:36 +0100

    Update ExcepcionErrorCero.java

--- src/exceptions/ExcepcionErrorCero.java
+++ src/exceptions/ExcepcionErrorCero.java
@@ -1,7 +1,7 @@
 package exceptions;
 
 public class ExcepcionErrorCero extends ExcepcionParametroNoValido {
-
+	private static final long serialVersionUID = 1L;
 	public ExcepcionErrorCero() {
 		super("El valor es cero y es invalido");
 	}
