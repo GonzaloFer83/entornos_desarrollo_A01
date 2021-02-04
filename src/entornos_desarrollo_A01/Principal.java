c3f72cd15b746e2b72b4f83a90ce09f6aa9e177c
GonzaloFer83 <54637433+GonzaloFer83@users.noreply.github.com>
2021-02-04 11:45:13 +0100

    ultima actualizacion

--- src/entornos_desarrollo_A01/Principal.java
+++ src/entornos_desarrollo_A01/Principal.java
@@ -28,5 +28,4 @@ public class Principal {
 			 System.out.println(error.getMessage());
 		 }
 	}
-
 }
--- src/exceptions/ExceptionExpAlto.java
+++ src/exceptions/ExceptionExpAlto.java
@@ -3,8 +3,13 @@ package exceptions;
 public class ExceptionExpAlto extends ExcepcionParametroNoValido {
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * Se implementa un error especifico para que no acepte el valores muy elevados
+	 * para el exponente
+	 * 
+	 */
 	public ExceptionExpAlto(double newExpo) {
-		super(newExpo +" es mas alto que el MAX_EXPONENT permitido");
+		super(newExpo + " es mas alto que el MAX_EXPONENT permitido");
 	}
 
 }
