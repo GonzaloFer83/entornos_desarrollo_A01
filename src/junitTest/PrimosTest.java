1640ccde7abcb7f1384759c52c9fad665997623a
GonzaloFer83 <54637433+GonzaloFer83@users.noreply.github.com>
2021-02-19 18:36:08 +0100

    testexception

--- src/junitTest/PrimosTest.java
+++ src/junitTest/PrimosTest.java
@@ -3,6 +3,7 @@ package junitTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,24 +28,29 @@ class PrimosTest {
 	void testEsPrimo() throws ExcepcionNumeroNegativo, ExcepcionErrorCero {
 		int primo = 7;
 		boolean esperado = true;
-		try {
-			boolean resultado = prim.esPrimo(primo);
-			assertEquals(esperado, resultado, "7 es un numero Primo");
-		} catch (ExcepcionParametroNoValido error) {
-			System.out.println(error.getMessage());
-		}
+		boolean resultado = prim.esPrimo(primo);
+		assertEquals(esperado, resultado, "7 es un numero Primo");
+		
 	}
+	@Test
+	void testValorZero() {
+		int primo = 0;
+		Assertions.assertThrows(ExcepcionErrorCero.class, () ->prim.iesimo(primo));
+		
+	}
+	@Test
+	void testValornegativo() {
+		int primo = -8;
+		Assertions.assertThrows(ExcepcionNumeroNegativo.class, () ->prim.iesimo(primo));
 
+		
+	}
 	@Test
 	void testIesimo() throws ExcepcionNumeroNegativo, ExcepcionErrorCero {
 		int primo = 7;
 		int esperado = 4;
-		try {
-			int resultado = prim.iesimo(primo);
-			assertEquals(esperado, resultado, "7 tiene 4 numeros primos delante");
-		}catch (ExcepcionParametroNoValido error) {
-			 System.out.println(error.getMessage());
-		 }
+		int resultado = prim.iesimo(primo);
+		assertEquals(esperado, resultado, "7 tiene 4 numeros primos delante");
 	}
 
 	@Test
@@ -52,23 +58,16 @@ class PrimosTest {
 		double valor1 = 100;
 		double valor2 = 20;
 		double esperado = 20;
-		try {
-			double resultado = prim.porcentaje(valor1, valor2);
-			assertEquals(esperado, resultado, "El 20 por ciento de 100 es 20");
-		}catch (ExcepcionParametroNoValido error) {
-			 System.out.println(error.getMessage());
-		 }
+		double resultado = prim.porcentaje(valor1, valor2);
+		assertEquals(esperado, resultado, "El 20 por ciento de 100 es 20");
+		
 	}
 
 	@Test
 	void testFactorial() throws ExcepcionNumeroNegativo, ExcepcionErrorCero {
 		int factorial = 5;
 		int esperado = 120;
-		try {
-			int resultado = prim.factorial(factorial);
-			assertEquals(esperado, resultado,"5! es 120");
-		}catch (ExcepcionParametroNoValido error) {
-			 System.out.println(error.getMessage());
-		 }
+		int resultado = prim.factorial(factorial);
+		assertEquals(esperado, resultado,"5! es 120");
 	}
 }
