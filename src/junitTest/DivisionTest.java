6c0925085c881f8d7551b9bef91bf63c3858298e
Afhsete12 <77535835+Afhsete12@users.noreply.github.com>
2021-02-24 15:13:34 +0100

    Update DivisionTest.java

--- src/junitTest/DivisionTest.java
+++ src/junitTest/DivisionTest.java
@@ -3,6 +3,7 @@ package junitTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,7 +26,8 @@ class DivisionTest {
 	}
 
 	@Test
-	void divisionRealEntre12_2_y_6_3DebeSer1_94() throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
+	void divisionRealEntre12_2_y_6_3DebeSer1_94()
+			throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
 		double dividiendo = 12.2;
 		double divisor = 6.3;
 		double esperado = 1.94;
@@ -34,26 +36,67 @@ class DivisionTest {
 	}
 
 	@Test
-	void laDivisionEnteraEntre12y6DebeSer2() throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
+	void laDivisionEnteraEntre12y6DebeSer2()
+			throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
 		int dividiendo = 12;
 		int divisor = 6;
 		int esperado = 2;
 		double obtenido = div.sumNumEnteros(dividiendo, divisor);
 		assertEquals(esperado, obtenido, "La division entre 12 y 6 debe ser 2");
 	}
+
 	@Test
-	void elValorInversoDe12Es0_08() throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
+	void elValorInversoDe12Es0_08()
+			throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
 		int numeroInverso = 12;
 		double esperado = 0.08;
 		double obtenido = div.inversoValor(numeroInverso);
 		assertEquals(esperado, obtenido, "El inverso de 12 ha de ser 0.08");
 	}
-	
+
 	@Test
-	void laRaizDe49DebeSer7() throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
+	void laRaizDe49DebeSer7()
+			throws ExcepcionErrorCero, ExcepcionNumeroNegativo, ExceptionNumeroAlto, ExcepcionNumeroMuyBajo {
 		int numeroRaiz = 49;
 		int esperado = 7;
 		double obtenido = div.raizDeNumero(numeroRaiz);
-		assertEquals(esperado, obtenido,"La raiz cuadrada de 49 debe ser 7");
+		assertEquals(esperado, obtenido, "La raiz cuadrada de 49 debe ser 7");
 	}
+
+	@Test
+	void testQuePasaSiDivisorEsCero() {
+		int dividendo = 12;
+		int divisor = 0;
+		Assertions.assertThrows(ExcepcionErrorCero.class, () -> div.sumNumEnteros(dividendo, divisor));
+	}
+
+	@Test
+	void testQuePasaSiUnoDeLosParametrosEsNegativo() {
+		int dividendo = 12;
+		int divisor = -5;
+		Assertions.assertThrows(ExcepcionNumeroNegativo.class, () -> div.sumNumEnteros(dividendo, divisor));
+	}
+
+	// Casos raros
+	@Test
+	void raizCuadradaNegativo() {
+		int numeroRaiz = -5;
+		Assertions.assertThrows(ExcepcionNumeroNegativo.class, () -> div.raizDeNumero(numeroRaiz));
+	}
+
+	@Test
+	void divisorMasAltoQueElTipoDatoPrimitivoPermitido()
+			throws ExceptionNumeroAlto, ExcepcionErrorCero, ExcepcionNumeroNegativo {
+		int dividiendo = 12;
+		Assertions.assertThrows(ExceptionNumeroAlto.class,
+				() -> div.divisionNumReales(dividiendo, Double.POSITIVE_INFINITY));
+	}
+
+	void divisorMasBajoQueElTipoDeDatoPrimitivo()
+			throws ExceptionNumeroAlto, ExcepcionErrorCero, ExcepcionNumeroNegativo {
+		int dividiendo = 12;
+		Assertions.assertThrows(ExceptionNumeroAlto.class,
+				() -> div.divisionNumReales(dividiendo, Double.POSITIVE_INFINITY));
+	}
+
 }
